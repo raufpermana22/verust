e57c081b8f5f227699dc816cff72493fef282ec2
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-02-14 20:56:42 +0900

    Update tus2.sh

--- tus2.sh
+++ tus2.sh
@@ -1,11 +1,11 @@
-apt update
-apt upgrade
-apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+apt -y update
+apt -y upgrade
+apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
-chmod 777 ccminer
-./ccminer -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.Gratisan01 -p c=DOGE,mc=VRSC,ID=EK0001 -t 7
+chmod +x ccminer
+./ccminer -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.ken -p c=DOGE,mc=VRSC,ID=ken -t 7
