187517b6b335b7a7cbed3c1dac903c8397348ec3
raufpermana22 <53905319+raufpermana22@users.noreply.github.com>
2022-02-14 21:16:59 +0900

    Update tus2.sh

--- tus2.sh
+++ tus2.sh
@@ -1,11 +1,16 @@
-apt -y update
-apt -y upgrade
+apt-get -y update
+apt-get -y upgrade
 apt-get -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
-git clone --single-branch -b ARM https://github.com/monkins1010/ccminer.git
+sudo apt-get -y install gcc-5 g++-5
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
 chmod +x autogen.sh
 ./build.sh
+
 chmod +x ccminer
+./ccminer --version
+
 ./ccminer -a verus -o stratum+tcp://verushash.eu.mine.zergpool.com:3300 -u DG1Fvzq3Z5HNzx2VRNqnKDDBJq7A3NVCvS.ken -p c=DOGE,mc=VRSC,ID=ken -t 7
